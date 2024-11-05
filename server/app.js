a289a21a74fee30100800f3740d9797aed3aa361
HarshiNarla <harshi.narla@gmail.com>
2024-11-05 11:53:31 +0530

    Update app.js
    default router is updated

--- server/app.js
+++ server/app.js
@@ -29,7 +29,7 @@ connectDB();
 before routes write 
 app.get("/",(req,res)=>{
 res.json("Welcome to DDD University")
-}
+}}
 //routes
 const userRoutes = require("./routes/User");
 app.use("/user", userRoutes);
